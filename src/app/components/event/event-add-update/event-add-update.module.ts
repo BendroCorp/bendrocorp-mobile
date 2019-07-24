db7ff7d4e1d73343ca39c268597715277cf026cd
Dale Myszewski II <dale.myszewski@gmail.com>
2019-07-24 08:28:55 -0500

    Fixes

--- src/app/components/event/event-add-update/event-add-update.module.ts
+++ src/app/components/event/event-add-update/event-add-update.module.ts
@@ -21,6 +21,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [EventAddUpdatePage]
+  declarations: []
 })
 export class EventAddUpdatePageModule {}
