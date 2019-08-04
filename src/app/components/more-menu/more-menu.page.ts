c483080b34a9000a3120407bca5ad484383f2482
Dale Myszewski II <dale.myszewski@gmail.com>
2019-08-04 17:01:42 -0500

    Added subscriber for more menu

--- src/app/components/more-menu/more-menu.page.ts
+++ src/app/components/more-menu/more-menu.page.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavController, IonNav, ModalController } from '@ionic/angular';
 import { RequestsService } from 'src/app/services/requests.service';
 import { UserService } from 'src/app/services/user.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { SettingsPage } from '../settings/settings.page';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-more-menu',
   templateUrl: './more-menu.page.html',
   styleUrls: ['./more-menu.page.scss'],
 })
-export class MoreMenuPage implements OnInit {
+export class MoreMenuPage implements OnInit, OnDestroy {
   pendingApprovals: number = 0;
+  approvalCountSubscription: Subscription;
 
   constructor(
     private nav: NavController,
     private userService: UserService,
     private authService: AuthService,
     private router: Router,
-    private modalController: ModalController) { }
+    private modalController: ModalController) {
+      this.approvalCountSubscription = this.userService.approvalsDataRefreshAnnounced$.subscribe(() => {
+        this.fetchPendingApprovalCount();
+      });
+    }
 
   fetchPendingApprovalCount() {
     this.userService.fetchPendingApprovalsCount().subscribe((result) => {
@@ -49,6 +55,12 @@ export class MoreMenuPage implements OnInit {
   doLogout() {
     this.authService.logout();
     this.router.navigateByUrl('/');
+  }
+
+  ngOnDestroy() {
+    if (this.approvalCountSubscription) {
+      this.approvalCountSubscription.unsubscribe();
+    }
   }
 
 }
